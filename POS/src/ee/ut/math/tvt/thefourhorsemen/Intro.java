087919f26617761586b0143a5e10f3a7ecc3703c
Arcticold <rasmus254@gmail.com>
2013-10-12 11:35:29 +0300

    First testing with JFrame

--- POS/src/ee/ut/math/tvt/thefourhorsemen/Intro.java
+++ POS/src/ee/ut/math/tvt/thefourhorsemen/Intro.java
@@ -1,10 +1,17 @@
 package ee.ut.math.tvt.thefourhorsemen;
 
-public class Intro {
+public class Intro 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		IntroUI.Window();
+		// see asi sai vist küll väga ümber nurga nüüd, aga 
+		// ei tahtnud mittetöötavat asja üles laadida.
+		// Proovisin küll extendida IntroUI'd ennast JFrame-iga, aga
+		// siis hakkasid igasugused static errorid tulema...
+		// Võite proovida ise, äkki teil rohkem õnne ja teete midagi 
+		// teistmoodi kui mina
 	}
 
 }
