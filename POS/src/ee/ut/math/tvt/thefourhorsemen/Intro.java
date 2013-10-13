9ea329c04c6213578fed81beebd293e0b3dff3c6
taivop <taivo.pungas@gmail.com>
2013-10-14 02:21:51 +0300

    Finalised build.xml. Added required log4j functionality.

--- POS/src/ee/ut/math/tvt/thefourhorsemen/Intro.java
+++ POS/src/ee/ut/math/tvt/thefourhorsemen/Intro.java
@@ -1,11 +1,18 @@
 package ee.ut.math.tvt.thefourhorsemen;
 
+import org.apache.log4j.Logger;
+
+
 public class Intro 
 {
 
 	public static void main(String[] args) 
 	{
+		// Initialise the logger
+		Logger log = Logger.getLogger(Intro.class);
+		
 		IntroUI.Window();
+		log.info("UI Window started!");
 		// see asi sai vist küll väga ümber nurga nüüd, aga 
 		// ei tahtnud mittetöötavat asja üles laadida.
 		// Proovisin küll extendida IntroUI'd ennast JFrame-iga, aga
