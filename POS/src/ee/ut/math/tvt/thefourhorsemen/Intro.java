e93a619b5e0ddab6030f10b06143c53fa1ea46ec
AndoDaBouss <Ando@172.17.53.91>
2013-10-25 12:31:20 +0300

    Fixed Intro.

--- POS/src/ee/ut/math/tvt/thefourhorsemen/Intro.java
+++ POS/src/ee/ut/math/tvt/thefourhorsemen/Intro.java
@@ -4,21 +4,58 @@ import java.io.IOException;
 
 import org.apache.log4j.Logger;
 
+import ee.ut.math.tvt.salessystem.domain.controller.SalesDomainController;
+import ee.ut.math.tvt.salessystem.domain.controller.impl.SalesDomainControllerImpl;
+import ee.ut.math.tvt.salessystem.ui.ConsoleUI;
+import ee.ut.math.tvt.salessystem.ui.SalesSystemUI;
+
 
 public class Intro 
 {
+	private static final Logger log = Logger.getLogger(Intro.class);
+	private static final String MODE = "console";
+
 
 	public static void main(String[] args) 
 	{
 		// Initialise the logger
 		Logger log = Logger.getLogger(Intro.class);
-		
+		/*
 		try {
 			IntroUI.Window();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		*/
+		
+		final SalesDomainController domainController = new SalesDomainControllerImpl();
+
+			if (args.length == 1 && args[0].equals(MODE)) {
+				log.debug("Mode: " + MODE);
+
+				ConsoleUI cui = new ConsoleUI(domainController);
+				cui.run();
+			} else {
+
+				try {
+					IntroUI.Window();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				
+			final SalesSystemUI ui = new SalesSystemUI(domainController);
+				ui.setVisible(true);
+
+				try {
+					Thread.sleep(3000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+
+		
 		
 		log.info("UI Window started!");
 		
