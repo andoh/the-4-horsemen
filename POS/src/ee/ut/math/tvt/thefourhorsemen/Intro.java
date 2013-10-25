764d1be676a7817bf38ceb112386ea5f22e12d06
Arcticold <rasmus254@gmail.com>
2013-10-25 11:52:18 +0300

    commit test

--- POS/src/ee/ut/math/tvt/thefourhorsemen/Intro.java
+++ POS/src/ee/ut/math/tvt/thefourhorsemen/Intro.java
@@ -1,17 +1,25 @@
 package ee.ut.math.tvt.thefourhorsemen;
 
+
 import java.io.IOException;
 
 import org.apache.log4j.Logger;
 
+import ee.ut.math.tvt.salessystem.domain.controller.SalesDomainController;
+import ee.ut.math.tvt.salessystem.domain.controller.impl.SalesDomainControllerImpl;
+import ee.ut.math.tvt.salessystem.ui.ConsoleUI;
+import ee.ut.math.tvt.salessystem.ui.SalesSystemUI;
 
 public class Intro 
 {
-
+	
+	private static final Logger log = Logger.getLogger(Intro.class);
+	private static final String MODE = "console";
+	
 	public static void main(String[] args) 
 	{
 		// Initialise the logger
-		Logger log = Logger.getLogger(Intro.class);
+		//Logger log = Logger.getLogger(Intro.class);
 		
 		try {
 			IntroUI.Window();
@@ -22,6 +30,38 @@ public class Intro
 		
 		log.info("UI Window started!");
 		
-	}
+		final SalesDomainController domainController = new SalesDomainControllerImpl();
 
+		if (args.length == 1 && args[0].equals(MODE)) {
+			log.debug("Mode: " + MODE);
+
+			ConsoleUI cui = new ConsoleUI(domainController);
+			cui.run();
+		} else {
+
+			//Enne oli IntroUI
+			
+			IntroUI introUI = new IntroUI();
+			try {
+				introUI.Window();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			//introUI.setVisible(true);
+			//introUI.setAlwaysOnTop(true);
+			
+			final SalesSystemUI ui = new SalesSystemUI(domainController);
+			ui.setVisible(true);
+
+			//introUI.setAlwaysOnTop(false);
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			//introUI.setVisible(false);
+		}
+	
+	}
 }
