5838042179751de3d2e0b296c4439c58a78129b2
Arcticold <rasmus254@gmail.com>
2013-11-23 21:59:29 +0200

    Created one test under StockItemTest. Test fails even though the error log itself shows equal instances - screw logic

--- POS/test/ee/ut/math/tvt/salessystem/domain/data/StockItemTest.java
+++ POS/test/ee/ut/math/tvt/salessystem/domain/data/StockItemTest.java
@@ -4,12 +4,24 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
 import org.junit.Test;
+import ee.ut.math.tvt.salessystem.domain.data.StockItem;
 
 public class StockItemTest {
 	
+	private StockItem item1, item2;
+	
+	@Before
+	public void setUp() {
+		item1 = new StockItem((long) 101, "Testiasi", "Blabla", 13, 10);
+		item2 = new StockItem(item1);
+	}
+	
+	@Test
 	public void testClone() {
 		// Me tegelt ei kloonigi mitte kunagi. Selle asemel on meil StockItemis konstruktor
 		// StockItem(StockItem sti), ehk siis testida tuleks just seda
+		assertEquals(item1, item2);
+		
 	}
 	
 	public void testGetColumn() {
